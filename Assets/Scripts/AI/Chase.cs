879a7ee97076ae6bf9de66f181ed905d764ba585
Paul Sinnett <paul.sinnett@gmail.com>
2020-11-29 17:59:05 +0000

    Initial commit

---
+++ Assets/Scripts/AI/Chase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    public float attackRange;
+    public float aiMoveSpeed;
+    
+
+    NavMeshAgent agent;
+    Detection detection;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        detection = GetComponent<Detection>();
+    }
+
+    void Update()
+    {
+        if(detection.target != null)
+        {
+            if(detection.rangeToTarget < attackRange)
+            {
+
+            }
+            else
+            {
+                agent.SetDestination(detection.target.position);
+            }
+        }
+        else
+        {
+            SendMessage("SetBehaviour", "Investigate");
+        }
+
+    }
+
+    public void ChasePlayer()
+    {
+        transform.Translate(Vector3.forward * aiMoveSpeed * Time.deltaTime);
+    }
+
+}
